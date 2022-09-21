da194ccd00859ad30d0db5f12bec7725c511b364
Gizmo770 <71050882+Gizmo770@users.noreply.github.com>
2022-09-21 01:38:05 -0500

    Level Design Tutorial

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,12 +9,21 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField]
     [Range(1,15)]
     float speed;
+    public float jumpHeight;
     Rigidbody2D playerRB;
     // Start is called before the first frame update
     void Start()
     {
         //Retrieve Components
         playerRB = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            playerRB.AddForce(Vector2.up * jumpHeight, ForceMode2D.Impulse);
+        }
     }
 
     // Update is called once per frame
