710bb3d83d6cfafbbad164039bc86c9a5c995674
Bennett <112603507+PeeBean@users.noreply.github.com>
2022-09-23 00:28:00 -0500

    Updated Spawner Script
    Works good

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -4,42 +4,67 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-
-    //SerializeField float freq; How often object will spawn
-    //int time/count -variable
-
     public GameObject obj;
  
-    [SerializeField] int time;
     [SerializeField] int freq;
     [SerializeField] int limit;
     [SerializeField] int distanceTillSpawn;
-    private int spawns;
+    [SerializeField] bool unlimited;
 
+    private int spawns;
+    private int time;
+    private float pos;
+    private float spawnerPos;
+    private bool init;
+
+    //Start is called before the first frame update
     void Start()
     {
-        
+     
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Check if count/time variable is divisile by freq
-        //Spawn in specified object
+        //Player Position and Spawner position are assaigned to corresponsing variables
+        pos = FindObjectOfType<PlayerMovement>().transform.position.x;
+        spawnerPos = transform.position.x;
 
-        //To find player gameobject and its location
-        if ((FindObjectOfType<PlayerMovement>().transform.position.x + distanceTillSpawn> obj.transform.position.x) && spawns<limit)
-        {
-            GameObject spawner = (GameObject)Instantiate(obj, transform.position, transform.rotation);
-            spawns++;
+        //If X value of player in addition to the specified distance is greater than the spawners position, Initialize the spawner
+        if (pos + distanceTillSpawn> spawnerPos)
+        { 
+            if (!init) //Used to only initialize the spawner once
+            {
+                //Using a unity method Invoke repeating to call the UpdateTime function every 1 second.
+                //UpdateTime() function is below the Update() function.
+                InvokeRepeating("UpdateTime", 1f, 1f);
+                GameObject spawner = (GameObject)Instantiate(obj, transform.position, transform.rotation);
+                spawns++;
+            }
+            init = true;
         }
 
-        if (Input.GetKeyDown(KeyCode.X) && spawns<limit)
+        //Actual Spawning process
+        if(init && time != 0 && time % freq == 0 && unlimited)
         {
             GameObject spawner = (GameObject)Instantiate(obj, transform.position, transform.rotation);
+            time = 0;
             spawns++;
         }
-        
+        else if(init && time != 0 && time % freq == 0 && spawns<limit)
+        {
+            GameObject spawner = (GameObject)Instantiate(obj, transform.position, transform.rotation);
+            time = 0;
+            spawns++;
+        }
+       
+        //To do something when key pressed if (Input.GetKeyDown(KeyCode.X))
     }
+
+    void UpdateTime()
+    {
+        //Updating the time variable every second by using InvokeRepeating in start method.
+        time++;
+    }
+
 }
