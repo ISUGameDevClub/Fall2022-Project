54b08a147dc375b886b4c8518fe5c883ca84d6e0
Cody Doerr (Pian) <cody.doerr@yahoo.com>
2022-09-25 11:05:28 -0500

    ?

--- Assets/playermovement.cs
+++ Assets/playermovement.cs
@@ -2,11 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class playermovement : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
 
     [SerializeField] float speed;
-
+    Rigidbody2D player;
     // Start is called before the first frame update
     void Start()
     {
