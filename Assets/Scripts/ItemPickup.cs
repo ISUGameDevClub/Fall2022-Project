3b2d785ef6d99c59bfb8311524c97b27940f9c04
codydoerr <40671276+codydoerr@users.noreply.github.com>
2022-10-09 23:27:09 -0500

    stole things from scenes and updated prefabs, finished week 1 dev room
    Dev room is ready, just added some stuff from prefabs stolen from other scenes, come and check out the dev room

--- Assets/Scripts/ItemPickup.cs
+++ Assets/Scripts/ItemPickup.cs
@@ -2,10 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class ItemPickup: MonoBehaviour
 {
     private bool hasHat;
-
+    public string pickupType;
     Rigidbody playerRB;
     Rigidbody hatPickup; 
 
