a29dcbbd79504e85af606887b53378d9d347440a
JodoDraggo <59331197+JodoDraggo@users.noreply.github.com>
2022-10-12 21:56:29 -0500

    Enemy Ranged Attack
    *Created a generic enemy object for testing*
    
    The enemy will shoot a projectile towards the player when in range.
    BUGS: the bullet range is the same as a melee hit, so checking "Is Trigger" on the Collider kills the player.

--- Assets/Scripts/EnemyAttack.cs
+++ Assets/Scripts/EnemyAttack.cs
@@ -2,30 +2,50 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//This script is for the enemy's 'ranged' attack.
+//The player's health script already takes care of enemy melee.
+
+//TESTING: Made and attached this script to "GenericEnemy" object for testing.
+
+//Still need to make bullet object invisible then make it visible when it is instantiated and shot.
+//Need to despawn objects a few seconds after they've been shot or when they collide with the player.
+//Collider says "no camera" when "IS TRIGGER" is on because the player dies. Need to distinguish the boxcollider of a melee attack vs the range of a bullet. 
+
 public class EnemyAttack : MonoBehaviour
 {
-    Ray ray;
-    // Start is called before the first frame update
-    void Awake()
+    public float speed = 2.1f, fireRate = 1.0f, fireDelay = 0.0f;
+    //public Rigidbody2D rb;
+    public Transform weapon; //the weapon the bullet is firing from
+    public GameObject bullet; //The 'projectile' prefab
+    public Transform player;
+
+    void Start()
     {
-        //Vector2 range = new Vector2(-20, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-
-
+        //Fire();
     }
 
-    void FireRay()
+    private void Fire()
     {
-        ray = new Ray(transform.position, transform.forward * 10);
-        RaycastHit hitData;
-        if (Physics.Raycast(ray, out hitData, 10))
+        if(Time.time > fireDelay)
         {
-            Debug.Log("Hit ray");
-            //work in progress
+            fireDelay = Time.time + fireRate; //fireRate instantiates as making the enemy fire every 1 second.
+            Vector2 myPos = new Vector2(weapon.position.x, weapon.position.y);
+            GameObject projectile = Instantiate(bullet, myPos, Quaternion.identity);
+            Vector2 direction = (Vector2)player.position - myPos; //get the direction of the player.
+            projectile.GetComponent<Rigidbody2D>().velocity = direction * speed; //shoot projectile
+        }
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            Fire();
         }
     }
 
--- Assets/Scripts/EnemyMelee.cs
+++ Assets/Scripts/EnemyMelee.cs
@@ -7,7 +7,7 @@ public class EnemyMelee : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -15,4 +15,19 @@ public class EnemyMelee : MonoBehaviour
     {
 
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            /*If a player is wearing a hat, make them lose the hat.
+             * If a player !wearing a hat, they die
+             * collision.gameObject.GetComponent<Health>().
+             */
+
+            //looks like Player's health script actually takes care of the enemy melee
+            
+        }
+    }
+
 }
