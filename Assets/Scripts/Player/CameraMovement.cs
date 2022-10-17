f69ee0f91dae4362f918ed40848d071f0dd77618
Wumboly <jackson.gorczowski@outlook.com>
2022-10-17 14:39:42 -0500

    Updated Camera Movement

--- Assets/Scripts/Player/CameraMovement.cs
+++ Assets/Scripts/Player/CameraMovement.cs
@@ -9,21 +9,41 @@ public class CameraMovement : MonoBehaviour
 
     [SerializeField]
     float cameraSpeed;
+    
+    [SerializeField]
+    bool useCameraStartPos;
+    
+    [SerializeField]
+    Vector2 cameraStartPos;
+
+    Vector3 startPos;
 
     [SerializeField]
     Vector2 posOffset;
 
     [SerializeField]
-    float maxHeightPos;
+    float minX;
 
     [SerializeField]
-    float minHeightPos;
+    float maxX;
+
+    [SerializeField]
+    float minY;
+
+    [SerializeField]
+    float maxY;
 
     // Start is called before the first frame update
     void Start()
     {
+        startPos = new Vector3(cameraStartPos.x, cameraStartPos.y, -1);
         player = GameObject.Find("Player");
-        transform.position = new Vector3(player.transform.position.x + posOffset.x, player.transform.position.y + posOffset.y, -1);
+
+        if (useCameraStartPos)
+            transform.position = startPos;
+
+        else
+            transform.position = new Vector3(player.transform.position.x + posOffset.x, player.transform.position.y + posOffset.y, -1);
     }
 
     // Update is called once per frame
@@ -35,21 +55,29 @@ public class CameraMovement : MonoBehaviour
         //cameras current pos
         Vector3 endPos = player.transform.position;
         endPos.x += posOffset.x;
-        endPos.y = (transform.position.y);
+        endPos.y += posOffset.y;
         endPos.z = -1;
 
-        if (player.transform.position.x + posOffset.x >= transform.position.x)
+        if (endPos.x <= minX)
+            endPos.x = minX;
+
+        if (endPos.x >= maxX)
+            endPos.x = maxX;
+
+        if (player.transform.position.x + posOffset.x < transform.position.x)
         {
-            transform.position = Vector3.Lerp(startPos, endPos, cameraSpeed * Time.deltaTime);
+            endPos.x = transform.position.x;
         }
 
+        if (endPos.y <= minY)
+            endPos.y = minY;
+
+        if (endPos.y >= maxY)
+            endPos.y = maxY;
+       
+        transform.position = Vector3.Lerp(startPos, endPos, cameraSpeed * Time.deltaTime);
         
 
-        if (!(player.transform.position.y >= (maxHeightPos)) && !(player.transform.position.y <= (minHeightPos)))
-        {
-            transform.position = new Vector3(transform.position.x, player.transform.position.y + posOffset.y, -1);
-
-        }
 
     }
 }
