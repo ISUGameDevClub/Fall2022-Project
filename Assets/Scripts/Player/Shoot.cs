9bee629f32d7ec1ea51e7eb00a88f688171380d0
Wumboly <jackson.gorczowski@outlook.com>
2022-11-02 21:52:51 -0500

    General Update
    Fixed bug with spider where it wouldn't damage player.
    Finished up glitch gun and is now fully functional.
    Added Boolean to one way platform to make it force the player to fall trough after standing on it for a certain time. Created "specials" for each weapon. By default each special is just its default projectile. Created a separate cooldown for specials.

--- Assets/Scripts/Player/Shoot.cs
+++ Assets/Scripts/Player/Shoot.cs
@@ -9,18 +9,22 @@ public class Shoot : MonoBehaviour
     Health playerHP;
 
     public float shotDelayValue = .05f;
+    public float specialShotDelayValue = 1;
     private bool canShootNow;
+    private bool specialCanShootNow;
 
     // Start is called before the first frame update
     void Start()
     {
         playerHP = transform.parent.GetComponent<Health>();
         canShootNow = true;
+        specialCanShootNow = true;
     }
 
     // Update is called once per frame
     void Update()
     {
+        //left click
         if(Input.GetMouseButton(0) && canShootNow == true)
         {
             canShootNow = false;
@@ -110,6 +114,97 @@ public class Shoot : MonoBehaviour
             }
         }
 
+        //Right Click
+        if (Input.GetMouseButton(1) && specialCanShootNow == true)
+        {
+            specialCanShootNow = false;
+            Vector2 position = gameObject.transform.position;
+            int bulletToSpawn = 10;
+            if (playerHP.playerHealth.Equals("bouncer"))
+            {
+                bulletToSpawn = 11;
+            }
+            else if (playerHP.playerHealth.Equals("juggernaut"))
+            {
+                bulletToSpawn = 12;
+            }
+            else if (playerHP.playerHealth.Equals("glitchgun"))
+            {
+                bulletToSpawn = 13;
+            }
+            else if (playerHP.playerHealth.Equals("windbreaker"))
+            {
+                bulletToSpawn = 14;
+            }
+            else if (playerHP.playerHealth.Equals("chillout"))
+            {
+                bulletToSpawn = 15;
+            }
+            else if (playerHP.playerHealth.Equals("yarnwhip"))
+            {
+                bulletToSpawn = 16;
+            }
+            else if (playerHP.playerHealth.Equals("moab"))
+            {
+                bulletToSpawn = 17;
+            }
+            else if (playerHP.playerHealth.Equals("snowbomb"))
+            {
+                bulletToSpawn = 18;
+            }
+            else if (playerHP.playerHealth.Equals("candrew"))
+            {
+                bulletToSpawn = 19;
+            }
+
+            //spawn bullet here
+            GameObject bullet = Instantiate(bulletPrefab[bulletToSpawn], position, Quaternion.identity);
+
+            StartCoroutine(specialShotDelay());
+
+            //make bullet fly forward
+            if (bulletToSpawn == 10)
+            {
+                bullet.GetComponent<Projectile_Player>().gun = gameObject;
+            }
+            if (bulletToSpawn == 11)
+            {
+                bullet.GetComponent<Projectile_Player>().gun = gameObject;
+            }
+            if (bulletToSpawn == 12)
+            {
+                bullet.GetComponent<Projectile_Player>().gun = gameObject;
+            }
+            if (bulletToSpawn == 13)
+            {
+                bullet.GetComponent<GlitchGunProjectile>().gun = gameObject;
+            }
+            if (bulletToSpawn == 14)
+            {
+                bullet.GetComponent<Projectile_Player>().gun = gameObject;
+            }
+            if (bulletToSpawn == 15)
+            {
+                bullet.GetComponent<Projectile_Player>().gun = gameObject;
+            }
+            if (bulletToSpawn == 16)
+            {
+                bullet.GetComponent<Projectile_Player>().gun = gameObject;
+            }
+            if (bulletToSpawn == 17)
+            {
+                bullet.GetComponent<Projectile_Player>().gun = gameObject;
+            }
+            if (bulletToSpawn == 18)
+            {
+                bullet.GetComponent<Projectile_Player>().gun = gameObject;
+            }
+            if (bulletToSpawn == 19)
+            {
+                bullet.GetComponent<Grenade_Player>().gun = gameObject;
+            }
+        }
+
     }
 
 
@@ -117,5 +212,11 @@ public class Shoot : MonoBehaviour
     {
         yield return new WaitForSeconds(shotDelayValue);
         canShootNow = true;
+    }
+
+    private IEnumerator specialShotDelay()
+    {
+        yield return new WaitForSeconds(specialShotDelayValue);
+        specialCanShootNow = true;
     }
 }
