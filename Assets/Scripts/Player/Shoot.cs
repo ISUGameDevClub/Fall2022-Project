46503a2f363d35c8077a29139f659f1c25161f82
ScepCat25 <113559595+ScepCat25@users.noreply.github.com>
2022-11-02 21:57:14 -0500

    Mostly Finished Juggernaut shooting
    Mostly Finished Juggernaut shooting, It now shoots all three angles, but the angles still need to be tightened.

--- Assets/Scripts/Player/Shoot.cs
+++ Assets/Scripts/Player/Shoot.cs
@@ -81,12 +81,10 @@ public class Shoot : MonoBehaviour
                 GameObject bullet2 = Instantiate(bulletPrefab[bulletToSpawn], position, Quaternion.identity );
                 GameObject bullet3 = Instantiate(bulletPrefab[bulletToSpawn], position,Quaternion.identity );
                 bullet.GetComponent<projectile_player_juggernaut>().gun = gameObject;
-                Quaternion.SetFromToRotation(new Vector2(1,1),new Vector2(1,-1));
                 bullet2.GetComponent<projectile_player_juggernaut>().gun = gameObject;
+                bullet2.GetComponent<projectile_player_juggernaut>().shotOrder = "Top";
                 bullet3.GetComponent<projectile_player_juggernaut>().gun = gameObject;
-                bullet.GetComponent<projectile_player_juggernaut>().gun.transform.localPosition = new Vector2(1, 1);
-                bullet2.GetComponent<projectile_player_juggernaut>().gun.transform.localPosition = new Vector2(1, 1);
-                bullet3.GetComponent<projectile_player_juggernaut>().gun.transform.localPosition = new Vector2(1, 1);
+                bullet3.GetComponent<projectile_player_juggernaut>().shotOrder = "Bottom";
             }
             if (bulletToSpawn == 3)
             {
--- Assets/projectile_player_juggernaut.cs
+++ Assets/projectile_player_juggernaut.cs
@@ -16,6 +16,7 @@ public class projectile_player_juggernaut : MonoBehaviour
     public float range = 10; 
     Vector2 startPosition;
     weaponType weapon;
+    public string shotOrder;
 
     // Start is called before the first frame update
     void Start()
@@ -34,46 +35,134 @@ public class projectile_player_juggernaut : MonoBehaviour
         // needs to despawn bullets after distance
         // needs to call grenade methods when nessary
        
-        if (aim == new Vector2(1, 0))
+        if (aim == new Vector2(0.78f, 0.413f))
         { 
+            if(shotOrder=="Top")
+            {
+                transform.Translate(movement/10, movement / 40, 0); 
+            }
+            else if(shotOrder=="Bottom")
+            {
+                transform.Translate(movement / 10, -movement / 40, 0);
+            }
+            else{
             //shoot right 
             transform.Translate(movement / 10, 0, 0);
+            }
             
         }
         else if (aim == new Vector2(0, 1))
         {
-            //shoot up
-            transform.Translate(0, movement / 10, 0); 
+             if(shotOrder=="Top")
+            {
+                transform.Translate(-movement/40, movement / 10, 0); 
+            }
+            else if(shotOrder=="Bottom")
+            {
+                transform.Translate(movement / 40, movement / 10, 0);
+            }
+            else{
+            //shoot up 
+            transform.Translate(0, movement / 10, 0);
+            }
+            
+             
         }
-        else if (aim == new Vector2(-1, 0))
+        else if (aim == new Vector2(-0.78f, 0.413f))
         {
+             if(shotOrder=="Top")
+            {
+                transform.Translate(-movement/10, movement / 40, 0); 
+            }
+            else if(shotOrder=="Bottom")
+            {
+                transform.Translate(-movement / 10, -movement / 40, 0);
+            }
+            else{
             //shoot left
             transform.Translate(-movement / 10, 0, 0);
+            }
+
         }
         else if (aim == new Vector2(0, -1))
         {
+            if(shotOrder=="Top")
+            {
+                transform.Translate(movement/40, -movement / 10, 0); 
+            }
+            else if(shotOrder=="Bottom")
+            {
+                transform.Translate(-movement / 40, -movement / 10, 0);
+            }
+            else{
             //shoot down
             transform.Translate(0, -movement / 10,  0);
+            }
+
         }
-        else if (aim == new Vector2(1, 1))
+        else if (aim == new Vector2(1, 1)) //Need to tighten the bullets for all angles below
         {
+            if(shotOrder=="Top")
+            {
+                transform.Translate(0, movement / 10, 0); 
+            }
+            else if(shotOrder=="Bottom")
+            {
+                transform.Translate(movement / 10, 0, 0);
+            }
+            else{
             //shoot top right
             transform.Translate(movement/10, movement / 10, 0);
+            }
+
         }
         else if (aim == new Vector2(-1, 1))
         {
+            if(shotOrder=="Top")
+            {
+                transform.Translate(0, movement / 10, 0); 
+            }
+            else if(shotOrder=="Bottom")
+            {
+                transform.Translate(-movement / 10, 0, 0);
+            }
+            else{
             //shoot top left
             transform.Translate(-movement / 10, movement / 10, 0);
+            }
+
         }
         else if (aim == new Vector2(1, -1))
         {
+            if(shotOrder=="Top")
+            {
+                transform.Translate(movement/10, 0, 0); 
+            }
+            else if(shotOrder=="Bottom")
+            {
+                transform.Translate(0, -movement / 10, 0);
+            }
+            else{
             //shoot bottom right
             transform.Translate(movement / 10, -movement / 10, 0);
+            }
+
         }
         else if (aim == new Vector2(-1, -1))
         {
+            if(shotOrder=="Top")
+            {
+                transform.Translate(-movement/10, 0, 0); 
+            }
+            else if(shotOrder=="Bottom")
+            {
+                transform.Translate(0, -movement / 10, 0);
+            }
+            else{
             //shoot bottom left
             transform.Translate(-movement / 10, -movement / 10, 0);
+            }
+
         }
         //despawn bullet after distance
 //        if (Vector2.Distance(startPosition, transform.position) > range) 
