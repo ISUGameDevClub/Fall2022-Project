fe796f25fcaf667b3cb1c8c92a9d882c087e8f84
Fallenangel700 <90366792+Fallenangel700@users.noreply.github.com>
2022-11-02 21:51:08 -0500

    Can work and MOAB Start
    Not sure can is 100% and MOAB is absolutely not.

--- Assets/Scripts/Projectile_Player.cs
+++ Assets/Scripts/Projectile_Player.cs
@@ -6,14 +6,15 @@ public class Projectile_Player : MonoBehaviour
 {
 
     public float movement = .5f;
+    public float despawnTime = .5f;
     public GameObject gun;
     private Vector2 aim; 
-    public float range = 10; 
     Vector2 startPosition;
 
     // Start is called before the first frame update
     void Start()
     {
+        Destroy(gameObject, despawnTime);
         aim = gun.transform.localPosition;
         Vector2 startPosition = transform.position;
     }
@@ -21,7 +22,6 @@ public class Projectile_Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // needs to despawn bullets after distance
 
         if (aim == new Vector2(0.78f, 0.413f))
         {
@@ -65,11 +65,6 @@ public class Projectile_Player : MonoBehaviour
             transform.Translate(-movement / 10, -movement / 10, 0);
         }
 
-        //despawn bullet after distance
-        //        if (Vector2.Distance(startPosition, transform.position) > range) 
-        //      {
-        //        Destroy(gameObject);
-        //  }
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
