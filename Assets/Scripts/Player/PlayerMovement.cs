074fcaf7437335411d494ca9f1fca0678686549f
bhayden51 <bradley8451@hotmail.com>
2022-11-09 11:50:17 -0600

    ground changes

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -29,6 +29,8 @@ public class PlayerMovement : MonoBehaviour
     Animator 
         upperBodyAnim;
 
+    private bool grounded;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -40,13 +42,13 @@ public class PlayerMovement : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && jumpsAvailable != jumps && !Input.GetKey(KeyCode.LeftShift))
+        if (Input.GetKeyDown(KeyCode.Space) && jumpsAvailable != jumps && (!Input.GetKey(KeyCode.LeftShift) || !grounded))
         {
             playerRB.AddForce(Vector2.up * jumpHeight, ForceMode2D.Impulse);
             jumps++;
             Instantiate(jumpPrefab, transform.position, Quaternion.identity);
         }
-        if (Input.GetAxisRaw("Horizontal") != 0 && !Input.GetKey(KeyCode.LeftShift)) 
+        if (Input.GetAxisRaw("Horizontal") != 0 && (!Input.GetKey(KeyCode.LeftShift) || !grounded)) 
         {
             lowerBodyAnim.SetBool("walking", true);
         }
@@ -54,7 +56,23 @@ public class PlayerMovement : MonoBehaviour
         {
             lowerBodyAnim.SetBool("walking", false);
         }
-        if (playerRB.velocity.y == 0) jumps = 0;
+
+        LayerMask mask = LayerMask.GetMask("Ground");
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 1.1f, mask);
+
+        if (LayerMask.LayerToName(hit.collider.gameObject.layer) == "Ground")
+        {
+            if (playerRB.velocity.y <= 0)
+            {
+                jumps = 0;
+                grounded = true;
+            }
+            playerRB.velocity = new Vector2(0, playerRB.velocity.y);
+        }
+        else
+        {
+            grounded = false;
+        }
 
         doWeFlip();
     }
@@ -62,7 +80,7 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (!Input.GetKey(KeyCode.LeftShift))
+        if ((!Input.GetKey(KeyCode.LeftShift) || !grounded))
         {
             Vector2 playerVelocity = new Vector2(Input.GetAxis("Horizontal") * speed, 0) * Time.fixedDeltaTime;
             playerRB.position += playerVelocity;
