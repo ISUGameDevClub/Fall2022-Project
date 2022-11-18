8400b96169e1bb4c563596a492bac37a98c1a90b
Gizmo770 <71050882+Gizmo770@users.noreply.github.com>
2022-11-18 15:37:06 -0600

    BoxSpawner
    Added Box Spawner

--- Assets/Scripts/Enemy/BoxSpawner.cs
+++ Assets/Scripts/Enemy/BoxSpawner.cs
@@ -5,23 +5,28 @@ using UnityEngine;
 public class BoxSpawner : MonoBehaviour
 {
     public GameObject enemyToSpawn;
-    public GameObject player;
+    private GameObject player;
     public float distanceTrigger;
-    public float spawnWaitTime;
-    public Animator boxAnimator;
+    public float spawnWaitTime = 1.5f;
+    private Animator boxAnimator;
+    private bool isSpawning;
+    private ParticleSystem particleSystem;
 
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
         boxAnimator = GetComponent<Animator>();
+        isSpawning = false;
+        particleSystem = GetComponentInChildren<ParticleSystem>();
     }
 
     // Update is called once per frame
     void Update()
     {
-       if(Vector2.Distance(this.transform.position, player.transform.position) < distanceTrigger)
+       if(Vector2.Distance(this.transform.position, player.transform.position) < distanceTrigger && !isSpawning)
        {
+            isSpawning = true;
             StartCoroutine(SpawnEnemies(spawnWaitTime));
        }
     }
@@ -30,8 +35,14 @@ public class BoxSpawner : MonoBehaviour
     {
         while (waitTime > 0)
         {
-            Instantiate(enemyToSpawn, new Vector3(transform.position.x - 5, transform.position.y, transform.position.z), Quaternion.identity);
+            boxAnimator.SetTrigger("SpawnEnemy");
             yield return new WaitForSeconds(waitTime);
         }
+    }
+
+    private void SpawnEnemy()
+    {
+        particleSystem.Play();
+        Instantiate(enemyToSpawn, new Vector3(transform.position.x - 2, transform.position.y, transform.position.z), Quaternion.identity);
     }
 }
