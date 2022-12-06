1d08b5384958924108cef3fc94d83a7a29cb8e07
bhayden51 <bradley8451@hotmail.com>
2022-12-06 00:54:27 -0600

    final polishes

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public bool isPaused;
+    public GameObject pauseMenu;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 1;
+        pauseMenu.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isPaused)
+        {
+            Time.timeScale = 0;
+        }
+
+        if(!isPaused && Input.GetKeyDown(KeyCode.Escape))
+        {
+            PauseGame();
+        }
+        else if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            UnpauseGame();
+        }
+    }
+
+    public void PauseGame()
+    {
+        isPaused = true;
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    public void UnpauseGame()
+    {
+        isPaused = false;
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    public void ReturnToTitle(string titleName)
+    {
+        FindObjectOfType<SceneTransition>().ChangeScene(titleName);        
+    }
+}
